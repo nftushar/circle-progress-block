7269096a25e21c7a19d174ad14c8eeaf4bcc3656
NF Tushar <nftushar5200@gmail.com>
2023-09-04 13:20:06 +0600

    Show percentage Done

--- src/Style.js
+++ src/Style.js
@@ -1,26 +1,20 @@
 const Style = ({ attributes, clientId }) => {
     const { alignment, size, elements } = attributes;
-    const {showPercentage} = elements;
-console.log(showPercentage);
+    const { showPercentage } = elements; 
     const mainSl = `#bBlocksCircleProgress-${clientId}`;
 
-    return (
-        <style
-            dangerouslySetInnerHTML={{
-                __html: `
-            ${mainSl} .bBlocksCircleProgress {
-                text-align: ${alignment};
-            }
-            ${mainSl} .bBlocksCircleProgress .circleProgress{
-                width: ${size}px;
-                height: ${size}px;
-            }
-            ${mainSl} .bBlocksCircleProgress .circleProgress strong{ 
-                display: ${showPercentage ? 'none' : 'block'}  }} 
-            }
-        }`,
-            }}
-        />
-    );
+    return <style dangerouslySetInnerHTML={{
+        __html: `
+        ${mainSl} .bBlocksCircleProgress {
+            text-align: ${alignment};
+        }
+        ${mainSl} .bBlocksCircleProgress .circleProgress{
+            width: ${size}px;
+            height: ${size}px;
+        }
+        ${mainSl} .bBlocksCircleProgress .circleProgress strong{
+            display: ${showPercentage ? 'none' : 'inline-block'};
+        }
+    `}} />;
 };
 export default Style;
